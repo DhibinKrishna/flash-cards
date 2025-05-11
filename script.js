9506c362f8cd8be98b665b805c1694032508231e
dhibin krishna <dhibink@gmail.com>
2025-05-11 17:13:24 +0530

    Colors added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentIndex = 0;
-const carousel = document.querySelector(".carousel");
+const carousel = document.getElementById("carousel");
 let touchStartX = 0;
 let touchEndX = 0;
 const swipeThreshold = 50;
@@ -82,6 +82,64 @@ const data = {
       alt: "Giraffe",
     },
   ],
+  colours: [
+    {
+      code: "#FF0000",
+      name: "Red",
+    },
+    {
+      code: "#00FF00",
+      name: "Green",
+    },
+    {
+      code: "#0000FF",
+      name: "Blue",
+    },
+    {
+      code: "#FFFF00",
+      name: "Yellow",
+    },
+    {
+      code: "#FFFFFF",
+      name: "White",
+    },
+    {
+      code: "#000000",
+      name: "Black",
+    },
+    {
+      code: "#808080",
+      name: "Grey",
+    },
+    {
+      code: "#FF7518",
+      name: "Orange",
+    },
+    {
+      code: "#800080",
+      name: "Purple",
+    },
+    {
+      code: "#FFC0CB",
+      name: "Pink",
+    },
+    {
+      code: "#FF9A8A",
+      name: "Peach",
+    },
+    {
+      code: "#B2A4D4",
+      name: "Lavender",
+    },
+    {
+      code: "#8B4513",
+      name: "Brown",
+    },
+    {
+      code: "#00FFFF",
+      name: "Cyan",
+    },
+  ],
 };
 
 carousel.addEventListener("touchstart", (e) => {
@@ -102,22 +160,33 @@ function selectCategory(selectedCategory) {
 }
 
 function initialize() {
-  const img = document.createElement("img");
   const title = document.getElementById("title");
-  img.id = "img";
-  img.src = data[category][0].src;
-  img.alt = data[category][0].alt;
-  img.onload = () => {
-    title.innerHTML = data[category][0].alt;
-  };
-  carousel.appendChild(img);
+
+  if (category === "colours") {
+    const colourDiv = document.createElement("div");
+    colourDiv.id = "colourDiv";
+    colourDiv.style.backgroundColor = data[category][0].code;
+    carousel.appendChild(colourDiv);
+    title.innerHTML = data[category][0].name;
+  } else {
+    const img = document.createElement("img");
+    img.id = "img";
+    img.src = data[category][0].src;
+    img.alt = data[category][0].alt;
+    img.onload = () => {
+      title.innerHTML = data[category][0].alt;
+    };
+    carousel.appendChild(img);
+  }
 }
 
 function goHome() {
   document.getElementById("home").style.display = "flex";
   document.getElementById("content").style.display = "none";
   const img = document.getElementById("img");
-  img.remove();
+  img?.remove();
+  const colourDiv = document.getElementById("colourDiv");
+  colourDiv?.remove();
   const title = document.getElementById("title");
   title.innerHTML = "";
   currentIndex = 0;
@@ -132,18 +201,25 @@ function moveSlide(direction) {
     currentIndex = data[category].length - 1;
   }
 
-  const img = document.getElementById("img");
   const title = document.getElementById("title");
-  img.style.opacity = "0.5";
-  title.style.opacity = "0.5";
 
-  img.src = data[category][currentIndex].src;
-  img.alt = data[category][currentIndex].alt;
-  img.onload = () => {
-    img.style.opacity = "1";
-    title.style.opacity = "1";
-    title.innerHTML = data[category][currentIndex].alt;
-  };
+  if (category === "colours") {
+    const colourDiv = document.getElementById("colourDiv");
+    colourDiv.style.backgroundColor = data[category][currentIndex].code;
+    title.innerHTML = data[category][currentIndex].name;
+  } else {
+    const img = document.getElementById("img");
+    img.style.opacity = "0.5";
+    title.style.opacity = "0.5";
+
+    img.src = data[category][currentIndex].src;
+    img.alt = data[category][currentIndex].alt;
+    img.onload = () => {
+      img.style.opacity = "1";
+      title.style.opacity = "1";
+      title.innerHTML = data[category][currentIndex].alt;
+    };
+  }
 }
 
 function handleSwipe() {
